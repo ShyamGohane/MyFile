9f7c299649b5cb425d65c6f03c82ac8cdd593e77
Your Name <you@example.com>
2022-12-23 11:33:32 +0530

    add Myfile.java Scanner class

---
+++ MyFile/src/com/MyFile/MyFile.java
@@ -0,0 +1,19 @@
+package com.MyFile;
+
+import java.util.Scanner;
+
+public class MyFile {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the username");
+		String username=sc.next();
+		System.out.println("Enter the password");
+		String password=sc.next();
+		
+		System.out.println("Ssuccessfull.......");
+	}
+
+	
+}
